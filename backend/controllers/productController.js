dac158462ece25ecfdc804118f8def2bdf51a339
Huynh Phuc <huynhhuuphuc100599@gmail.com>
2022-03-14 15:13:48 +0700

    Error handling

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,48 +1,46 @@
 const Product = require("../models/productModel");
+const ErrorHander = require("../utils/errorhander");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 // [POST] Create Product -- Admin
-exports.createProduct = async (req, res, next) => {
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
 
   res.status(201).json({
     success: true,
     product,
   });
-};
+});
 
 // [GET] Get all product
-exports.getAllProducts = async (req, res) => {
+exports.getAllProducts = catchAsyncErrors(async (req, res) => {
   const products = await Product.find();
 
   res.status(200).json({ success: true, products });
-};
+});
 
 // [GET] Get product details
-exports.getProductDetails = async (req, res, next) => {
+exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product not found",
-    });
+    return next(new ErrorHander("Product no found", 404));
   }
+
   res.status(200).json({
     success: true,
     product,
   });
-};
+});
 
 // [PUT] Update product -- Admin
-exports.updateProduct = async (req, res, next) => {
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product not found",
-    });
+    return next(new ErrorHander("Product no found", 404));
   }
+
   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
@@ -51,22 +49,20 @@ exports.updateProduct = async (req, res, next) => {
     success: true,
     product,
   });
-};
+});
 
 // [DELETE] Delete product
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Delete khong thanh cong",
-    });
+    return next(new ErrorHander("Product no found", 404));
   }
+
   await Product.deleteOne({ _id: req.params.id }).then(() =>
     res.status(200).json({
       success: true,
       message: "Product Deleted",
     })
   );
-};
+});
--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,8 +4,14 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Interal Server Error";
 
+  // Wrong Mongodb Id error (id url wrong)
+  if (err.name === "CastError") {
+    const message = `Resourse not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     succes: false,
-    err: err,
+    error: err,
   });
 };
